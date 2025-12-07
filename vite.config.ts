3702de6986165120bd040ddb65ff2540874a0fbb
Sine <lijuzhe@outlook.com>
2025-12-07 16:48:26 +0800

    增加编译压缩

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import components from 'unplugin-vue-components/vite'
 import autoImport from 'unplugin-auto-import/vite'
 import { VarletImportResolver } from '@varlet/import-resolver'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +15,30 @@ export default defineConfig({
     }),
     autoImport({
       resolvers: [VarletImportResolver({ autoImport: true }),ElementPlusResolver()],
+    }),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+      filename: 'dist/stats.html'
     })
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'echarts': ['echarts'],
+          'opencv': ['@techstark/opencv-js'],
+          'vendor': ['vue', 'vue-router', 'vue-i18n', '@varlet/ui'],
+          'ui-extra': ['element-plus', '@element-plus/icons-vue'],
+        }
+      }
+    },
+    minify: 'terser',
+    terserOptions: ({ 
+      compress: {
+        drop_console: true
+      }
+    } as any)
+  }
 })
